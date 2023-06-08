5bb920815b70d67975f9b207c21d8a95e8227f7f
mahesh190495 <58552773+mahesh190495@users.noreply.github.com>
2023-06-08 12:54:02 +0530

    Update get-latest-helm-version.ps1

--- windows_cmd/get-latest-helm-version.ps1
+++ windows_cmd/get-latest-helm-version.ps1
@@ -1,5 +1,9 @@
-$apiUrl = 'https://api.github.com/repos/helm/helm/releases/latest'
-$response = Invoke-RestMethod -Uri $apiUrl
-$latestVersion = $response.tag_name
-$ENV:LATEST_VERSION = $latestVersion
-Write-Output "Helm latest version: $latestVersion"
+if [ '${{ inputs.version }}' == "latest" ]; then
+  $apiUrl = 'https://api.github.com/repos/helm/helm/releases/latest'
+  $response = Invoke-RestMethod -Uri $apiUrl
+  $latestVersion = $response.tag_name
+  $ENV:LATEST_VERSION = $latestVersion
+  Write-Output "Helm latest version: $latestVersion"
+else:
+  $latestVersion='${{ inputs.version }}'
+  
